1c55365933131bd14cd7799b68eaa313b64ef32d
Edd Twilbeck <etwilbeck@marketingincolor.com>
2018-01-25 12:19:12 -0600

    Update template-alt-default.php

--- themes/Madico/template-alt-default.php
+++ themes/Madico/template-alt-default.php
@@ -37,39 +37,3 @@ get_header('madico');
 <?php
 get_footer('madico');
 ?>
-<script>
-	if (location.href != "http://dealerdirectory.madico.com/contact-us/#wpcf7-f720-p8-o1") {
-		jQuery('#country-select').prepend('<option value="" disabled selected>Select Country</option>');
-		jQuery('#state-select').prepend('<option value="" disabled selected>Select State</option>');
-		jQuery('#province-select').prepend('<option value="" disabled selected>Select Province</option>');
-		jQuery('input:not(#organization-input),textarea,#country-select').prop('required',true);
-	}
-
-	jQuery('#country-select').on('change',function(){
-		if(this.value == 'United States'){
-			jQuery('#state-select').prop('required',true);
-			jQuery('#province-select').prop('required',true);
-		}else if(this.value == 'Canada'){
-			jQuery('#province-select').prop('required',true);
-			jQuery('#state-select').prop('required',false);
-		}else{
-			jQuery('#province-select').prop('required',false);
-			jQuery('#state-select').prop('required',false);
-		}
-	});
-
-	jQuery('.wpcf7-submit').on('click',function(){
-		console.log('hello')
-		var inputs = jQuery('input:not(#organization-input),textarea,#country-select')
-		for(i = 0;i < inputs.length;i++){
-			if(inputs[i].hasAttribute("required")){
-	      if(inputs[i].value == ""){
-	          // found an empty field that is required
-	          alert("Please fill all required fields");
-	          return false;
-	      }
-	    }
-		}
-		return true;
-	});
-</script>
